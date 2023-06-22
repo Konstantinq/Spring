ae88df1de0d6052784c74bbec02933684f0022c4
ksolovev <q644070@yandex.ru>
2023-06-22 14:24:49 +0300

    not view info error input

--- Введение в Spring MVC (XML_based подход)/simple_mvc/src/main/java/org/example/web/dto/Book.java
+++ Введение в Spring MVC (XML_based подход)/simple_mvc/src/main/java/org/example/web/dto/Book.java
@@ -2,14 +2,15 @@ package org.example.web.dto;
 
 
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public class Book {
     private Integer id;
 
-    @NotNull
+    @NotBlank
     private String author;
-    @NotNull
+    @NotBlank
     private String title;
     @Digits(integer = 4, fraction = 0)
     private Integer size;
